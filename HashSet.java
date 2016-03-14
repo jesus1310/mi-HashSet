5a4fb631c9c0b22f7949b016d6aff9b32365835b
Jesus <jfr1013@gmail.com>
2016-03-14 12:25:46 +0100

    Primer commit

---
+++ HashSet.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class HashSet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HashSet
+{
+    /**
+     * Constructor for objects of class HashSet
+     */
+    public HashSet()
+    {
+        
+    }
+}
